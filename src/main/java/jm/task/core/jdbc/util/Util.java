c8d23aa2c4561488ffa1434b4bdbdff5710e87ff
Michael Saratovskiy <bigmsx@gmail.com>
2022-02-21 19:04:35 +0300

    Add Driver connection to util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -16,9 +16,10 @@ public class Util {
     public static Connection getConnection() {
 
         try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(URL,USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            System.out.println("Connection error: " + e);;
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Connection error: " + e);
         }
         return connection;
     }
